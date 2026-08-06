fbcf067a16d31e003306aa58adc1a1bc8a1fc008
van <van291630@gmail.com>
2026-08-06 13:54:43 +0300

    admin sign In page done

--- dashboard/src/pages/login/login.jsx
+++ dashboard/src/pages/login/login.jsx
@@ -1,8 +1,27 @@
 import "./login.scss";
+import abstaract from '../../assets/backgroundDecore.png'
+import { Link } from "react-router-dom";
 const Login = ()=>{
     return(
         <div className="login">
-            home
+            <div className="backgroundDecores">
+                <img src={abstaract} alt="" />
+            </div>
+           <div className="loginContainer">
+              <h2>Please fill in your unique admin login details below</h2>
+              <div className="theInputs">
+                <label >Email address</label>
+                <input type="email" />
+                <label >Password</label>
+                <input type="password" />
+                <label >ID No.</label>
+                <input type="text" />
+              </div>
+              <p className="doNotHaveAccount">Don't Have An Account? <Link style={{textDecoration:'none',color: '#000'}} >Sign Up</Link></p>
+              <button className="signUpButton">
+                Sign In
+              </button>
+           </div>
         </div>
     )
 }
